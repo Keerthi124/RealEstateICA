c665a191c81ab004fd5520af23327d730ef63f11
keerthi124 <keerthi124krishna@mgmail.com>
2020-10-16 21:46:07 +0800

    Add project files.

---
+++ RealEstateICA/Models/Branch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RealEstateICA.Models
+{
+    public class Branch
+    {
+        [Key]
+        public String BranchNo { get; set; }
+        public String Street { get; set; }
+        public String City { get; set; }
+        public int Postcode { get; set; }
+    }
+}
---
+++ RealEstateICA/Models/RealEsContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RealEstateICA.Models
+{
+    public class RealEsContext:DbContext
+    {
+        public DbSet<Branch> Branches { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Rent> Rents { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+    }
+}
